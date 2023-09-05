f294c943759208580c8709a0499b23c7efaaad45
Elizaveta Chervonnaya <ntropy@bk.ru>
2023-09-06 00:40:05 +0300

    add methods logic, DBConstants, Player entity annotations, pom dependencies, spy.properties and session factory configuration

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,23 +1,33 @@
 package com.game.entity;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
-
+@NamedQueries(
+        @NamedQuery(name = "getAllCount", query = "SELECT COUNT(*) FROM Player")
+)
+@Entity
+@Table(name = "player", schema = "rpg")
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(name = "name")
     private String name;
-
+    @Column(name = "title")
     private String title;
-
+    @Column(name = "race")
+    @Enumerated(EnumType.ORDINAL)
     private Race race;
-
+    @Column(name = "profession")
+    @Enumerated(EnumType.ORDINAL)
     private Profession profession;
-
+    @Column(name = "birthday")
     private Date birthday;
-
+    @Column(name = "banned")
     private Boolean banned;
-
+    @Column(name = "level")
     private Integer level;
 
     public Player() {
